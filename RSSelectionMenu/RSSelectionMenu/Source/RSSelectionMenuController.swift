3113b7fa7ee7adfc1d402edcd5a8b22b1aad2c68
Sam Flattery <samflattery@gmail.com>
2019-08-11 15:53:19 +0100

    Attributed string for alert title

--- RSSelectionMenu/RSSelectionMenu/Source/RSSelectionMenuController.swift
+++ RSSelectionMenu/RSSelectionMenu/Source/RSSelectionMenuController.swift
@@ -413,7 +413,6 @@ extension RSSelectionMenu {
             }
         }
         
-        from.present(tobePresentController, animated: true, completion: nil)
         from.present(tobePresentController, animated: true) {
             tobePresentController.view.superview?.subviews[0].isUserInteractionEnabled = false
         }
@@ -423,8 +422,15 @@ extension RSSelectionMenu {
     fileprivate func getAlertViewController(style: UIAlertController.Style, title: String?, action: String?, height: Double?) -> UIAlertController {
         let alertController = UIAlertController(title: title, message: nil, preferredStyle: style)
         
+        if let title = title {
+            var attributedTitle = NSMutableAttributedString()
+            attributedTitle = NSMutableAttributedString(string: title, attributes: [NSAttributedString.Key.font:UIFont(name: "IowanOldSt OSF BT", size: 18.0)!])
+            attributedTitle.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor(red: 166/255, green: 25/255, blue: 46/255, alpha: 1), range: NSRange(location:0,length:title.count))
+            alertController.setValue(attributedTitle, forKey: "attributedTitle")
+        }
+        
         let actionTitle = action ?? doneButtonTitle
-        let doneAction = UIAlertAction(title: actionTitle, style: .destructive) { [weak self] (doneButton) in
+        let doneAction = UIAlertAction(title: actionTitle, style: .cancel) { [weak self] (doneButton) in
             self?.menuWillDismiss()
         }
         
